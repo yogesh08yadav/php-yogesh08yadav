ae7886064da83e7a50e3778f4339dcd2e19dfa8b
yogesh08yadav <yogeshkumaras0808@gmail.com>
2021-09-14 22:11:57 +0530

    master

--- callapi.php
+++ callapi.php
@@ -8,12 +8,13 @@ echo $data;*/
 $json_url = "https://xkcd.com/info.0.json";
 $json = file_get_contents($json_url);
 $data1 = json_decode($json, TRUE);
+echo array($data1);
 $attach = $data1['img'];
 $img = 'images/img.png';
 
 $down = file_put_contents($img,$attach);
 
-print_r($attach);
+//print_r($attach);
 
 ////echo "<pre>";
 //print_r($attach);
